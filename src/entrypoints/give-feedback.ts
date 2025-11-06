6b325dd8539df404af1189c47437a40ffb197c8c
Nikita Barkov <nikita.barkov@jetbrains.com>
2025-11-06 10:53:46 +0100

    Fixed update review comment

--- src/entrypoints/give-feedback.ts
+++ src/entrypoints/give-feedback.ts
@@ -1,88 +1,32 @@
-import {createOctokit} from "../github/api/client";
 import * as core from "@actions/core";
-import {
-    PR_CREATED_FEEDBACK_COMMENT_TEMPLATE,
-    COMMIT_PUSHED_FEEDBACK_COMMENT_TEMPLATE,
-    SUCCESS_FEEDBACK_COMMENT_WITH_RESULT, MANUALLY_PR_CREATE_FEEDBACK_COMMENT_TEMPLATE, ERROR_FEEDBACK_COMMENT_TEMPLATE
-} from "../github/constants";
-import {ActionType} from "./handle-results";
+import {FinishFeedbackData, writeFinishFeedbackComment} from "../github/operations/comments/feedback";
 import {GitHubContext} from "../github/context";
-import {GITHUB_SERVER_URL} from "../github/api/config";
-import {createJobRunLink} from "../github/operations/comments/common";
+import {ActionType} from "./handle-results";
 
-function getFailedBody(owner: string, repoName: string, runId: string): string | undefined {
-    const details = process.env.ERROR || "Check job logs for more details"
-    const jobLink = createJobRunLink(owner, repoName, runId)
-    return `${ERROR_FEEDBACK_COMMENT_TEMPLATE(details, jobLink)}`;
-}
-
-function getSuccessBody(repoFullName: string): string | undefined {
-    const actionToDo = process.env.ACTION_TO_DO as keyof typeof ActionType;
-    const prLink = process.env.PR_LINK;
-    const commitSHA = process.env.COMMIT_SHA;
-    const junieTitle = process.env.JUNIE_TITLE;
-    const junieSummary = process.env.JUNIE_SUMMARY;
-    const workingBranch = process.env.WORKING_BRANCH!;
-    const baseBranch = process.env.BASE_BRANCH!;
-
-    let result: string | undefined;
-    switch (actionToDo) {
-        case "COMMIT_CHANGES":
-            console.log(`Commit pushed to current branch: ${commitSHA}`);
-            result = COMMIT_PUSHED_FEEDBACK_COMMENT_TEMPLATE(commitSHA!);
-            break;
-        case "CREATE_PR":
-            if (prLink) {
-                console.log(`PR was created: ${prLink}`);
-                result = PR_CREATED_FEEDBACK_COMMENT_TEMPLATE(prLink);
-            } else {
-                console.log(`Create PR manually`);
-                const createPRLink = `${GITHUB_SERVER_URL}/${repoFullName}/compare/${baseBranch}...${workingBranch}`;
-                result = MANUALLY_PR_CREATE_FEEDBACK_COMMENT_TEMPLATE(createPRLink);
-            }
-            break;
-        case "WRITE_COMMENT":
-            console.log('No PR or commit - using Junie result');
-            result = SUCCESS_FEEDBACK_COMMENT_WITH_RESULT(junieTitle || 'Task completed', junieSummary || 'No additional details');
-            break;
-    }
-
-    return result;
-}
 
 export async function giveFeedback() {
     try {
-        const isJobFailed = process.env.IS_JOB_FAILED === 'true';
-        const initCommentId = process.env.INIT_COMMENT_ID!;
-        const parsedContext = JSON.parse(process.env.PARSED_CONTEXT!) as GitHubContext;
-        const githubToken = process.env.GITHUB_TOKEN!;
-        const {owner, name} = parsedContext.payload.repository;
-        const ownerLogin = owner.login;
-        const repoFullName = `${ownerLogin}/${name}`;
-        let feedbackBody: string | undefined;
-        if (isJobFailed) {
-            feedbackBody = getFailedBody(ownerLogin, name, parsedContext.runId)
-        } else {
-            feedbackBody = getSuccessBody(repoFullName)
+        const data: FinishFeedbackData = {
+            githubToken: process.env.GITHUB_TOKEN!,
+            initCommentId: process.env.INIT_COMMENT_ID!,
+            isJobFailed: process.env.IS_JOB_FAILED === 'true',
+            parsedContext: JSON.parse(process.env.PARSED_CONTEXT!) as GitHubContext
+        }
+        if (data.isJobFailed) {
+            data.failureData = {error: process.env.ERROR}
+        } else{
+            data.successData = {
+                actionToDo: process.env.ACTION_TO_DO as keyof typeof ActionType,
+                baseBranch: process.env.BASE_BRANCH,
+                commitSHA: process.env.COMMIT_SHA,
+                junieSummary: process.env.JUNIE_SUMMARY,
+                junieTitle: process.env.JUNIE_TITLE,
+                prLink: process.env.PR_LINK,
+                workingBranch: process.env.WORKING_BRANCH
+            }
         }
 
-        if (!feedbackBody) {
-            console.log('No feedback body - skipping feedback');
-            return;
-        }
-
-        const octokit = createOctokit(githubToken);
-
-        // Update the initial comment with feedback
-        console.log(`Updating comment ${initCommentId} with feedback`);
-        await octokit.rest.issues.updateComment({
-            owner: ownerLogin,
-            repo: name,
-            comment_id: +initCommentId,
-            body: feedbackBody,
-        });
-
-        console.log('Feedback comment updated successfully');
+       await writeFinishFeedbackComment(data)
     } catch (error) {
         const errorMessage = error instanceof Error ? error.message : String(error);
         core.setFailed(`Give feedback step failed with error: ${errorMessage}`);
--- src/github/operations/comments/feedback.ts
+++ src/github/operations/comments/feedback.ts
@@ -3,10 +3,19 @@
 import * as core from "@actions/core";
 import {createJobRunLink, createCommentBody} from "./common";
 import {
+    GitHubContext,
     isPullRequestReviewCommentEvent,
     type ParsedGitHubContext,
 } from "../../context";
 import type {Octokit} from "@octokit/rest";
+import {createOctokit} from "../../api/client";
+import {
+    COMMIT_PUSHED_FEEDBACK_COMMENT_TEMPLATE,
+    ERROR_FEEDBACK_COMMENT_TEMPLATE, MANUALLY_PR_CREATE_FEEDBACK_COMMENT_TEMPLATE,
+    PR_CREATED_FEEDBACK_COMMENT_TEMPLATE, SUCCESS_FEEDBACK_COMMENT_WITH_RESULT
+} from "../../constants";
+import {ActionType} from "../../../entrypoints/handle-results";
+import {GITHUB_SERVER_URL} from "../../api/config";
 
 export async function writeInitialFeedbackComment(
     octokit: Octokit,
@@ -46,4 +55,100 @@ export async function writeInitialFeedbackComment(
         console.error("Error in initial comment:", error);
         throw error;
     }
+}
+
+
+export async function writeFinishFeedbackComment(data: FinishFeedbackData) {
+    const {owner, name} = data.parsedContext.payload.repository;
+    const ownerLogin = owner.login;
+    const repoFullName = `${ownerLogin}/${name}`;
+    let feedbackBody: string | undefined;
+    if (data.isJobFailed) {
+        feedbackBody = getFailedBody(ownerLogin, name, data.parsedContext.runId, data.failureData!)
+    } else {
+        feedbackBody = getSuccessBody(repoFullName, data.successData!)
+    }
+
+    if (!feedbackBody) {
+        console.log('No feedback body - skipping feedback');
+        return;
+    }
+
+    const octokit = createOctokit(data.githubToken);
+
+    const isReviewComment = isPullRequestReviewCommentEvent(data.parsedContext);
+    const initCommentId = data.initCommentId;
+    console.log(`Updating comment ${initCommentId} (review comment: ${isReviewComment})`);
+    if (isReviewComment) {
+        await octokit.rest.pulls.updateReviewComment({
+            owner: ownerLogin,
+            repo: name,
+            comment_id: +initCommentId,
+            body: feedbackBody,
+        });
+    } else {
+        await octokit.rest.issues.updateComment({
+            owner: ownerLogin,
+            repo: name,
+            comment_id: +initCommentId,
+            body: feedbackBody,
+        });
+    }
+
+    console.log('Feedback comment updated successfully');
+}
+
+export interface FinishFeedbackData {
+    initCommentId: string;
+    isJobFailed: boolean;
+    githubToken: string;
+    parsedContext: GitHubContext;
+    successData?: SuccessFeedbackData;
+    failureData?: FailureFeedbackData;
+}
+
+interface SuccessFeedbackData {
+    actionToDo: keyof typeof ActionType;
+    prLink?: string;
+    commitSHA?: string;
+    junieTitle?: string;
+    junieSummary?: string;
+    workingBranch?: string;
+    baseBranch?: string;
+}
+
+interface FailureFeedbackData {
+    error?: string;
+}
+
+function getFailedBody(owner: string, repoName: string, runId: string, failureData: FailureFeedbackData): string | undefined {
+    const details = failureData.error || "Check job logs for more details"
+    const jobLink = createJobRunLink(owner, repoName, runId)
+    return `${ERROR_FEEDBACK_COMMENT_TEMPLATE(details, jobLink)}`;
+}
+
+function getSuccessBody(repoFullName: string, successData: SuccessFeedbackData): string | undefined {
+    let result: string | undefined;
+    switch (successData.actionToDo) {
+        case "COMMIT_CHANGES":
+            console.log(`Commit pushed to current branch: ${successData.commitSHA}`);
+            result = COMMIT_PUSHED_FEEDBACK_COMMENT_TEMPLATE(successData.commitSHA!);
+            break;
+        case "CREATE_PR":
+            if (successData.prLink) {
+                console.log(`PR was created: ${successData.prLink}`);
+                result = PR_CREATED_FEEDBACK_COMMENT_TEMPLATE(successData.prLink);
+            } else {
+                console.log(`Create PR manually`);
+                const createPRLink = `${GITHUB_SERVER_URL}/${repoFullName}/compare/${successData.baseBranch}...${successData.workingBranch}`;
+                result = MANUALLY_PR_CREATE_FEEDBACK_COMMENT_TEMPLATE(createPRLink);
+            }
+            break;
+        case "WRITE_COMMENT":
+            console.log('No PR or commit - using Junie result');
+            result = SUCCESS_FEEDBACK_COMMENT_WITH_RESULT(successData.junieTitle || 'Task completed', successData.junieSummary || 'No additional details');
+            break;
+    }
+
+    return result;
 }
